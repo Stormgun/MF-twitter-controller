8f473892ada30d08ff3b6be4de773faeaebb4727
ISDADS\aaa48571 <angel.a.petrov2@gmail.com>
2018-09-06 13:34:27 +0100

    A hardcoded object that contains topics that act as super themes and themes . A semi natural language interpreter for tweets, making the user less burdened by rules when they tweet at MF. We currently keep having to create alias lists to lower the requirements on the user creating the tweet. In the future our main system should have a friendly title/alias to the actual naming of the scene. This doubles down for themes as well, until semantic queries start working and we can just send the tweets as a query.
    For this commit i've only completed the Hardcoded Ceramic aliasing GDC to follow.

--- src/js/index.js
+++ src/js/index.js
@@ -53,6 +53,63 @@ var IndustryScenes = [
     {sceneId: '5b27abba2bbfd6f81a934b9e', themes: ["Videos"]},
 ];
 
+
+var CeramicScenes = {
+    fire: {
+        people: {sceneId: '5b44d016330bc29c146dbc4c', theme: "Fire with People"},
+        furious: {sceneId: '5b44d04f330bc29c146dbc4d', theme: "Ferocious"},
+        background: {sceneId: '5b44cf47330bc29c146dbc49', theme: "Fire Background fast"}
+    },
+    water: {
+        running: {sceneId: '5b47754be4e95eac039228a1', theme: 'Running Water Overlays'},
+        drop: {sceneId: '5b477bd3e4e95eac039228a5', theme: 'Water Drop Overlays'}
+    },
+    kiln: {
+
+        smoke: {sceneId: '5b479424e4e95eac039228c0', theme: "Smoke"},
+        smash: {sceneId: '5b4796dfe4e95eac039228c5', theme: "Smash"},
+        pots: {sceneId: '5b4792d4e4e95eac039228bc', theme: "Overlay 3 Pots"},
+        lighting: {sceneId: '5b3e2354997f6d9c22e61667', theme: "Lighting"},
+        prep: {sceneId: '5b478873e4e95eac039228b0', theme: 'Wood Waiting'},
+    },
+    mountain: {
+        lifting: {sceneId: "5b45d992e4e95eac03922852", theme: "collecting"},
+        breaking: {sceneId: '5b44afd4330bc29c146dbc1d', theme: 'breaking'},
+        hands: {sceneId: '5b45d86fe4e95eac0392284e', theme: "hands"},
+        background: {
+            sceneId: '5b508286bfcf1b60279b6013',
+            theme: "All"
+        }
+    },
+    texture: {
+        smooth: {sceneid: '5b54dac5bfcf1b60279b6086', theme: "Smooth"},
+        rough: {sceneId: '5b54df28bfcf1b60279b608e', theme: "Rough"},
+        rustic: {sceneId: 'vis_v3_Texture_T7_Rustic', theme: "Rustic"},
+        brushstroke: {sceneId: '5b54e14fbfcf1b60279b6092', theme: "BrushStroke"},
+        moon: {
+            sceneId: '5b54e2c4bfcf1b60279b6095', theme: "Smooth moon"
+        }
+    },
+    wheel: {
+        detaching: {sceneId: '5b45e65be4e95eac03922865', theme: "Detaching"},
+        rolling: {sceneId: '5b45dbe3e4e95eac03922859', theme: "rolling"},
+        extruding: {sceneId: '5b45dd91e4e95eac0392285c', theme: "extruding"},
+    }
+};
+var GDCScenes = {
+    Dalian: [],
+    HongKong: [],
+    Seoul: [],
+    Changdu: [],
+    Chicago: [],
+    Manchester: [],
+    Shenyang: [],
+    Beijin: [],
+    KualaLampur: []
+
+};
+
+
 var options = {
     mimetypes: {json: ['application/json', 'application/json; charset=utf-8']},
 };
@@ -81,23 +138,97 @@ var checkForThemes = function (msg) {
         return {exists: false, theme: null};
     }
 };
-var CheckForKeyWords = function(text){
+var CheckForKeyWords = function (text) {
     var wordArray = text.split(/\s+/);
     var scenes = [];
     var themes = [];
+    var score = checkCeramic(wordArray);
+    scenes = _.uniq(score.scenes);
+    themes = score.themes;
+    console.log("angel",themes,scenes);
+    TwitterController.showScenesWithThemes(scenes, themes);
+};
+var checkGDC = function (wordArray) {
 
-    _.each(wordArray,function(word){
-        _.each(IndustryScenes,function(scene){
-            if(_.includes(scene.themes,word)){
+    var scenes = [];
+    var themes = [];
+
+    _.each(wordArray, function (word) {
+        if (word.toLowerCase() === "gdc") {
+            var idx = Math.floor(Math.random() * Object.keys(GDCScenes).length + 1)
+            var topic = Object.keys(GDCScenes)[idx];
+            var idxx = Math.floor(Math.random() * topic.length + 1)
+            scenes.push(topic[idxx]);
+
+        } else {
+            _.each(Object.keys(GDCScenes), function (topic) {
+                if (_.includes(topic.themesAliases, word)) {
+                    scenes.push(topic.sceneId);
+                    themes.push(topic.theme);
+                }
+            })
+        }
+
+        _.each(IndustryScenes, function (scene) {
+            if (_.includes(scene.themes, word)) {
                 scenes.push(scene.sceneId);
                 themes.push(word);
             }
         })
     });
-    scenes = _.uniq(scenes);
-    TwitterController.showScenesWithThemes(scenes,themes);
-};
+}
+var checkCeramic = function (wordArray) {
+try{
+    var scenes = [];
+    var themes = [];
 
+    _.each(wordArray, function (word) {
+        if (word.toLowerCase() === "ceramic") {
+            console.log("someone said ceramic")
+            var idx = Math.floor(Math.random() * Object.keys(CeramicScenes).length)
+            var topicKey = Object.keys(CeramicScenes)[idx];
+            var topicObj =CeramicScenes[topicKey];
+
+            console.log(topicObj,idx,Object.keys(CeramicScenes).length)
+            var idxx = Math.floor(Math.random() * Object.keys(topicObj).length)
+            var key = Object.keys(topicObj)[idxx];
+            var topicSubObj = topicObj[key];
+            scenes.push(topicSubObj.sceneId);
+            themes.push(topicSubObj.theme);
+
+        } else {
+            _.each(Object.keys(CeramicScenes), function (topic,i) {
+
+                if (topic === word.toLowerCase()) {
+                    var topicObj = CeramicScenes[topic];
+                    var idx = Math.floor(Math.random() * Object.keys(topicObj).length )
+                    var key = Object.keys(topicObj)[idx];
+                    var topicSubObj = topicObj[key];
+                    console.log("we matched with a topic", topicObj,topicSubObj)
+                    scenes.push(topicSubObj.sceneId);
+                    themes.push(topicSubObj.theme);
+
+                }
+                else if(_.includes(Object.keys(CeramicScenes[topic]), word.toLowerCase())) {
+                    var propTop = CeramicScenes[topic];
+                    console.log("We matched with a theme",word,propTop)
+                    scenes.push(propTop[word].sceneId);
+                    themes.push(propTop[word].theme);
+
+                }else{
+                    console.log("not match")
+                }
+            })
+        }
+
+    });
+    return {scenes:scenes,themes:themes}
+}catch(e){
+    console.log(e,"but we are still running")
+    return {scenes:[],themes:[]};
+}
+
+}
 var TwitterController = {
     authWithMF: function () {
         var self = this;
@@ -133,17 +264,19 @@ var TwitterController = {
                 "themes": []
             },
         };
-        socket.emit("sendCommand", "industry4", 'showScenesAndThemes', args);
+        socket.emit("sendCommand", "conference2018D", 'showScenesAndThemes', args);
+        socket.emit("sendCommand", "conference2018D", 'playScenesAndThemes', args);
     },
-    showScenesWithThemes:function(scenes,themes){
+    showScenesWithThemes: function (scenes, themes) {
         var args = {
             "play": {
-                "scenes":scenes,
+                "scenes": scenes,
                 "themes": themes
             }
         };
-        console.log("scenes and themes",args);
-        socket.emit("sendCommand", "industry4", 'showScenesAndThemes', args);
+        console.log("scenes and themes", args);
+        socket.emit("sendCommand", "conference2018D", 'showScenesAndThemes', args);
+        socket.emit("sendCommand", "conference2018D", 'playScenesAndThemes', args);
     },
     showSceneWithTheme: function (scene, themes, callback) {
         var args = {
@@ -156,7 +289,8 @@ var TwitterController = {
                 ]
             }
         };
-        socket.emit("sendCommand", "industry4", 'showScenesAndThemes', args);
+        socket.emit("sendCommand", "conference2018D", 'showScenesAndThemes', args);
+        socket.emit("sendCommand", "conference2018D", 'playScenesAndThemes', args);
     },
     findSceneByName: function (scene, theme) {
         var self = this;
@@ -194,6 +328,12 @@ var TwitterController = {
         });
         stream.on('error', function (error) {
             console.log(error);
+            if(error.statusCode === 420){
+                console.log("Returned when an app is being rate limited for making too many requests.")
+            }
+             client.get("application/rate_limit_status",{},function(err,data){
+                console.log(err,data)
+            })
         });
     }
 }
